efbd761e8ef6203f302c7edb28bedba9c5eb6cc4
farishuda <farishudaku@gmail.com>
2026-02-12 20:35:21 +0700

    chore: setup CI and remove wildcard imports

--- src/main/java/com/example/individualprep/controller/VectorController.java
+++ src/main/java/com/example/individualprep/controller/VectorController.java
@@ -5,7 +5,10 @@ import com.example.individualprep.dto.VectorResponse;
 import com.example.individualprep.service.VectorUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/vector")
