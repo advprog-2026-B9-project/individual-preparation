fa84cd623a43a3c85cd5e29510eba299f802a1e2
Faris Huda <150550342+hyude@users.noreply.github.com>
2026-02-12 21:49:28 +0700

    Revert "Setup CI"

--- src/test/java/com/example/individualprep/service/VectorUtilityTest.java
+++ src/test/java/com/example/individualprep/service/VectorUtilityTest.java
@@ -1,7 +1,7 @@
 package com.example.individualprep.service;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class VectorUtilityTest {
 
