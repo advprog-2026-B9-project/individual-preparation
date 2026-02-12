067cf6d1c7b2c5694c25b362868ccb3c1b6ad027
RafasyaSubhan <rafasya.subhan@gmail.com>
2026-02-12 18:28:06 +0700

    feat: implement vector add method

--- src/main/java/com/example/individualprep/service/VectorUtility.java
+++ src/main/java/com/example/individualprep/service/VectorUtility.java
@@ -6,8 +6,15 @@ import org.springframework.stereotype.Service;
 public class VectorUtility {
     
     public double[] add(double[] v1, double[] v2) {
-        // TODO: Implement me properly!
-        return new double[] { 0.0, 0.0, 0.0 };
+        if (v1.length != v2.length) {
+            throw new IllegalArgumentException("Vector lengths must be equal");
+        }
+
+        double[] result = new double[v1.length];
+        for (int i = 0; i < v1.length; i++) {
+            result[i] = v1[i] + v2[i];
+        }
+        return result;
     }
 
     public double[] subtract(double[] v1, double[] v2) {
