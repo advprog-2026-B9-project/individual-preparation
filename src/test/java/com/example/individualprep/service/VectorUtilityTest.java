f796e9bb071d365fe4294779f003a53c251c324f
anjeelika <azzahraanjelikaborselano@gmail.com>
2026-02-13 10:33:16 +0700

    test: add unit tests for vector subtract

--- src/test/java/com/example/individualprep/service/VectorUtilityTest.java
+++ src/test/java/com/example/individualprep/service/VectorUtilityTest.java
@@ -8,6 +8,32 @@ public class VectorUtilityTest {
     VectorUtility service = new VectorUtility();
 
     @Test
+    void testSubtractVectors() {
+        double[] vector1 = new double[]{1.0, 2.0, 3.0};
+        double[] vector2 = new double[]{1.0, 2.0, 3.0};
+
+        double[] resultVector = service.subtract(vector1, vector2);
+        double[] expectedVector = new double[]{0.0, 0.0, 0.0};
+
+        for (int index = 0; index < resultVector.length; index++){
+            assertEquals(expectedVector[index], resultVector[index]);
+        }
+    }
+
+    @Test
+    void testSubtractDifferentLengths() {
+        double[] vector1 = new double[]{1.0, 2.0, 3.0};
+        double[] vector2 = new double[]{1.0, 2.0};
+
+        try{
+            service.subtract(vector1, vector2);
+        }
+        catch(IllegalArgumentException e) {
+            assertEquals("Vector lengths must be equal", e.getMessage());
+        }
+    }
+
+    @Test
     void testMultiplyWithPositive(){
         double[] vector = new double[]{1.0, 2.0, 3.0};
         int scalar = 3;
